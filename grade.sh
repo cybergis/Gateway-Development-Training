659703e8ba67cfb4882aeec42d1be29e3d1b1144
Xingchen Hong <hello@xc-h.net>
2016-09-26 15:33:41 -0500

    Fix command breaking issue.

--- grade.sh
+++ grade.sh
@@ -7,16 +7,16 @@ printf 'test-----------------------------------\n'
 CURL_VERSION=$(curl --version 2>&1)
 CURL_VERSION_RC=$? # 0 when success, other values when error.
 
-printf 'curl>>%s\n' $CURL_VERSION
+printf 'curl>>%s\n' "${CURL_VERSION}"
 
 NODE_VERSION=$(node --version 2>&1)
 NODE_VERSION_RC=$? # 0 when success, other values when error.
 
-printf 'node>>%s\n' $NODE_VERSION
+printf 'node>>%s\n' "${NODE_VERSION}"
 
 NVM_VERSION=$(nvm --version 2>&1)
 NVM_VERSION_RC=$? # 0 when success, other values when error.
 
-printf 'nvm>>%s\n' $NVM_VERSION
+printf 'nvm>>%s\n' "${NVM_VERSION}"
 
 exit 1
