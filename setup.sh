7dcbb3e998c64c17bee081bb01bbfac393c3fe76
Ubuntu <huhao.nju@gmail.com>
2016-10-02 21:33:05 +0000

    hao's branch first commit

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 # Add your code here.
+
+# Download and install NVM
+curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
+
+source ~/.profile
+
+# Install and use Nodejs
+nvm install node
+nvm use node
+
+# Download and install meteor
+curl https://install.meteor.com/ | sh
+
+
