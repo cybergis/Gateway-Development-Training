944bbf4af1c0153b755a9b8c6a93e48ed7edba85
tsccsj <yizhaotsccsj@gmail.com>
2016-10-02 13:59:22 -0500

    Yizhao's submission

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 # Add your code here.
 sh meteor.sh
 export PATH=$PATH:$HOME/.meteor
-meteor create helloworld --release 1.4.1
-meteor create legacyapp --release 1.3.5
+
+#meteor create helloworld --release 1.4.1
+#meteor create legacyapp --release 1.3.5
