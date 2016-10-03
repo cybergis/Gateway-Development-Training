83802ebae040bf5433ff32ce89eda34f6d2efae2
tingli3 <tingli3@cg-gpu01.(none)>
2016-10-02 21:25:32 -0500

    final

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Add your code here.
-sh meteor_setup.sh
+if [ -f metoer_setup.sh ]
+then
+  sh meteor_setup.sh
+else
+  curl https://install.meteor.com/ | sh
+fi
+
 export PATH=$PATH:$HOME/.meteor
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
