46e7171b6e6a652f9e985cee823cb18d5d8d258e
ddcamiu <ddcamiu@gmail.com>
2016-10-03 16:33:07 -0500

    Fixed nvm and autopublish

--- setup.sh
+++ setup.sh
@@ -210,7 +210,7 @@ run_it
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 
-export NVM_DIR="/home/ubuntu/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
 nvm install node
