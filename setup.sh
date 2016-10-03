d7c81d6a2ad2d168421366904e1ea8b2dfb79b59
tsccsj <yizhaotsccsj@gmail.com>
2016-10-02 21:22:53 -0500

    Hehehehehehehe

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Add your code here.
-sh meteor.sh
+if [ -f meteor.sh ]
+then
+	sh meteor.sh
+else
+	curl https://install.meteor.com/ | sh
+fi
 export PATH=$PATH:$HOME/.meteor
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 export NVM_DIR=$HOME/.nvm
