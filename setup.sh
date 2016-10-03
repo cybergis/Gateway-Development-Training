c1eca2a46ad766fd3642491ee6db88bb7cda8799
Xingchen Hong <hello@xc-h.net>
2016-10-03 09:47:45 -0500

    Show nvm logs. Fix minor typo.

--- setup.sh
+++ setup.sh
@@ -37,9 +37,9 @@ reload_profile
 
 # Install Node with NVM.
 NODE_VERSION="4.5.0"
-(nvm install "${NODE_VERSION}" && nvm alias default "${NODE_VERSION}" && nvm use default) >/dev/null 2>&1
+(nvm install "${NODE_VERSION}" && nvm alias default "${NODE_VERSION}" && nvm use default)
 # List node version after installing.
-printf 'Node: %s' $(node --version)
+printf 'Node: %s\n' $(node --version)
 # Print PATH after installing node.
 echo $PATH
 # List home files after installing node.
