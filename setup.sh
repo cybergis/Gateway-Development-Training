80ffd6f64a1b1914307d6afe9006153bae855916
Xingchen Hong <hello@xc-h.net>
2016-10-03 09:49:33 -0500

    Random testing.

--- setup.sh
+++ setup.sh
@@ -37,7 +37,10 @@ reload_profile
 
 # Install Node with NVM.
 NODE_VERSION="4.5.0"
-(nvm install "${NODE_VERSION}" && nvm alias default "${NODE_VERSION}" && nvm use default)
+nvm install "${NODE_VERSION}"
+nvm alias default "${NODE_VERSION}"
+nvm use default
+
 # List node version after installing.
 printf 'Node: %s\n' $(node --version)
 # Print PATH after installing node.
