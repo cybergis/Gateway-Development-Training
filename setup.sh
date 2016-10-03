e3820fc495d867c9f6fc1c2e370bf54d51fa7b5e
tingli3 <tingli3@cg-gpu01.(none)>
2016-10-02 20:48:38 -0500

    submit

--- setup.sh
+++ setup.sh
@@ -4,3 +4,10 @@
 sh meteor_setup.sh
 export PATH=$PATH:$HOME/.meteor
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+export NVM_DIR=$HOME/.nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+nvm install node
+nvm use node
+
