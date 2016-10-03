e0fd50846b80234abfd8120e6fcaf93f4d52bee3
ddcamiu <ddcamiu@gmail.com>
2016-10-03 17:06:07 -0500

    Finalize

--- setup.sh
+++ setup.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 curl https://install.meteor.com/ | bash
 
-# add meteor bin to path
-export PATH=$HOME/.meteor/meteor:$PATH
-#echo "export PATH=\$HOME/.meteor/meteor:\$PATH" >> $HOME/.bashrc
+export PATH="$HOME/.meteor/meteor:$PATH"
 
-# install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-export NVM_DIR=$HOME/.nvm
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . $NVM_DIR/nvm.sh
-#echo "export NVM_DIR=$HOME/.nvm" >> $HOME/.bashrc
-#echo "[ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh"  >> $HOME/.bashrc
 
-# install nodejs
 nvm install node
 nvm alias default node
