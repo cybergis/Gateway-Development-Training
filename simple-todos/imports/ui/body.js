07b21fa7f2341b6ee636f6c432a17052a9b9281d
ncasler <ncasler@illinois.edu>
2016-10-28 21:42:20 -0500

    Step 4

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -8,7 +8,26 @@ import './body.html';
 
 Template.body.helpers({
     tasks() {
-            return Tasks.find({});
+            return Tasks.find({}, { sort: { createAt: -1});
     }
 });
 
+Template.body.events({
+    'submit .new-task'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        // Insert a task into the collection
+        Tasks.insert({
+            text,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        target.text.value = '';
+    },
+})
