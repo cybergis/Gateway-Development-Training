76bafedca2086fde7508e75f021e225fbea21200
ncasler <ncasler@illinois.edu>
2016-10-30 18:29:11 -0500

    Step Nine

--- simple-todos/imports/ui/task.js
+++ simple-todos/imports/ui/task.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 import { Tasks } from '../api/tasks.js';
@@ -7,12 +8,10 @@ import './task.html';
 Template.task.events({
     'click .toggle-checked'() {
         // set the checked property to the opposit of its current value
-        Tasks.update(this._id, {
-            $set: { checked: ! this.checked},
-        });
+        Meteor.call('tasks.setChecked', this._id, !this.checked);
     },
     'click .delete'() {
-        Tasks.remove(this._id);
+        Meteor.call('tasks.remove', this._id);
     },
 });
 
